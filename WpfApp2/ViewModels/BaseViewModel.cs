351f28a9468d7715a32782730fb2a59d46c6db1b
40338732 <40338732@users.noreply.github.com>
2020-05-29 19:49:09 +0100

    Wpf App Start up to point 9

---
+++ WpfApp2/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WpfApp2.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyCharged;
+
+        public void OnChanged(string propertyChanged)
+        {
+            PropertyCharged(this, new PropertyChangedEventArgs(propertyChanged));
+        }
+    }
+}
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
---
+++ WpfApp2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace WpfApp2.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+
+    }
+}
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
